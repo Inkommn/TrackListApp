cda722f5ec12be36c503f142a527ffa14cc08f1d
Shamkhan Mutuskhanov <inkomman@gmail.com>
2023-06-10 10:41:50 +0300

    Initial Commit

---
+++ TrackListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrackListApp
+//
+//  Created by Shamkhan Mutuskhanov on 10.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
